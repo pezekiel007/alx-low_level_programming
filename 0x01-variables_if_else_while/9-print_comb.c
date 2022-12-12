c7df0ea56583bbe0ce49c815a8a19300600be396
pezekiel007 <pezekiel00@gmail.com>
2022-12-12 08:04:01 -0800

    Task 9. Patience, persistence and perspiration make an unbeatable combination for success

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -7,19 +7,16 @@
  */
 int main(void)
 {
-	int a, b, c;
-
-	for (a = '0'; a <= '9'; a++)
-	{
-		putchar(a, ',');
-		putchar(' ');
-	}
+	int b;
+	int c;
 
 	for (b = '0'; b <= '9'; b++)
 	{
 		for (c = '0'; c <= '9'; c++)
 		{
-			putchar(b, c, ',');
+			putchar(b);
+			putchar(c);
+			putchar(',');
 			putchar(' ');
 		}
 	}
