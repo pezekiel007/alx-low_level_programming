18af0c50707e4f78e7bd224128080a7880b1267b
pezekiel007 <pezekiel00@gmail.com>
2022-12-12 06:33:54 -0800

    Task 8. Hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char ch;
+	int num;
+
+	for (num = 0; num <= 9; num++)
+		putchar(num);
+	for (ch = 'a'; ch <= 'f'; ch++)
+		putchar(ch);
+	putchar('\n');
+	return (0);
+}
