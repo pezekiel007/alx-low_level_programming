4e2e20425720172fb50e1c52b21dac573b95bbea
pezekiel007 <pezekiel00@gmail.com>
2022-12-12 17:32:04 -0800

    Task 10. Inventing is a combination of brains and materials.

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int x;
+	int y;
+
+	for (x = '0'; x <= '9'; x++)
+	{
+		for (y = '0'; y <= '9'; y++)
+		{
+			if (x == '1' && y == '0')
+			{
+				putchar(x);
+				putchar(y);
+				if (x != '9' && y != '9')
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
