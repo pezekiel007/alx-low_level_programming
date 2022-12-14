437e174406a162077b481e3bf1137123c31aea62
pezekiel007 <pezekiel00@gmail.com>
2022-12-13 17:55:27 -0800

    Task 1. I sometimes suffer from insomnia.

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void);
+{
+	int alphabet = 'a';
+
+	while (alphabet <= 'z')
+	{
+		_putchar(alphabet);
+	}
+	_putchar('\n');
+	return (0);
+}
