6b1c66fa960bd6411b76339479fd9232ae20f791
pezekiel007 <pezekiel00@gmail.com>
2022-12-13 18:14:51 -0800

    Task 1. I sometimes suffer from insomnia

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -5,14 +5,21 @@
  *
  * Return: Always 0 (Success)
  */
-int main(void)
+void print_alphabet(void);
 {
 	int alphabet = 'a';
 
 	while (alphabet <= 'z')
 	{
 		_putchar(alphabet);
+		alphabet += 1
 	}
 	_putchar('\n');
+	return (10);
+}
+
+int main(void)
+{
+	print_alphabet();
 	return (0);
 }
