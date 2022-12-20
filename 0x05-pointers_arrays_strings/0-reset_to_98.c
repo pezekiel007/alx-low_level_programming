2dd99a99943bbd2e84175e9edf4be78bf9ec36b0
pezekiel007 <pezekiel00@gmail.com>
2022-12-20 01:09:21 -0800

    0. 98 Battery st.

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -3,6 +3,7 @@
 /**
  * reset_to_98 - A function that takes a pointer to an int
  * as parameter and updates the value it points to to 98
+ * @n: The character to print
  *
  * Return: Always 0.
  */
