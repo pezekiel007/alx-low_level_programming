54650089e944864aa226545c3b091fad9120cd64
pezekiel007 <pezekiel00@gmail.com>
2022-12-21 07:11:41 -0800

    3. strcmp

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _strcmp - A function that compares two strings.
+ * @s1: An input string
+ * @s2: An input string
+ * Return: The difference s1 - s2
+ */
+int _strcmp(char *s1, char *s2)
+{
+	while (*s1 && *s2)
+	{
+		if (*s1 != *s2)
+			return (*s1 - *s2);
+		s1++, s2++;
+	}
+	return (*s1 - *s2);
+}
